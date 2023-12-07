77181b152be87178a01b676a5625e0da7c913cb9
Shobhit <shobhitshrivastava216@gmail.com>
2023-12-07 11:36:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from "react-router-dom";
+
+import {routes} from "./routes";
+
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                {routes.map(route => <Route {...route} />)}
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
---
+++ src/routes.js
@@ -0,0 +1,5 @@
+import Home from "./pages/Home";
+//list your routes here
+export const routes = [
+    {path: "/", element: <Home/>},
+]
